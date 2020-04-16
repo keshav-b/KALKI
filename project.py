bae5b15cd9292dfaf4ee763fee45f48b2e8689d9
Keshav Balachandar <30681515+keshav-b@users.noreply.github.com>
2020-04-16 10:51:33 +0530

    v2

--- project.py
+++ project.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
 from topic import topicExtract
+from sum import sumTopic
 
 app = Flask(__name__)
+
+para = []
+topic = []
 
 @app.route('/')
 def indexDisp():
@@ -9,16 +13,46 @@ def indexDisp():
 
 @app.route('/topics', methods = ['POST', 'GET'])
 def summaryTime():
+    global para 
+    global topic
     if request.method == 'POST':
-        topic = topicExtract(request.form.get("message"))
-        html = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Topics Available</title></head><body><h1>Topics : </h1>"
+        topic,para = topicExtract(request.form.get("message"))
+        html = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Topics Available</title></head><body><h1>Topics : </h1><br>"
         
-        for i in range (len(topic)):
-           html = html + "<h4>" + str(i) + ". " + str(topic[i]) + "<br></h4>"
+        html = html + "<form action=\"/summary\" method=\"POST\" name=\"topicsum\">"
 
-        html = html + "</body></html>"
+        for i in range (len(topic)):
+           html = html + "<input type=\"checkbox\" id=\"op" + str(i) + "\" name=\"op" + str(i) + "\" value=\"" + str(i) + "\"> " + str(topic[i]) + "<br>" 
+
+        html = html + "<br><br> Summary Level <br><input name=\"thres\" id=\"thres\" type=\"range\" min=\"1\" max=\"101\" value=\"50\" step=\"25\"><br><br>"
+
+        html = html + "0 &nbsp 25 &nbsp 50 &nbsp 75 &nbsp 100 <br><br>"
+
+        html = html + "<button name=\"forwardBtn\" onclick=\"move_forward()\">Submit</button></form></body></html>"
 
         return (html)
+
+@app.route('/summary', methods = ['POST', 'GET'])
+def summ():
+    if request.method == 'POST':
+        data = ''
+        for i in range (len(topic)-1):
+            op = "op" + str(i)
+            val1 = request.form.get(op)
+            if val1 == str(i):
+                data = data + para[i]
+            val2 = int(request.form.get('thres'))
+
+    length = len(data.split('.'))
+    thres = int((length/100)*val2)
+    if thres == 0:
+        thres = 1
+    summary = sumTopic(data,thres)
+
+    html = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Summary</title></head><body><h1>Your Summary : </h1><br><h4>" + str(summary) + "</h4>"
+    html = html + "</body></html>"
+    return(str(html))
+
 
 
 
